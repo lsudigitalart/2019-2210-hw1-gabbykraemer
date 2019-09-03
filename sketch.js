4856173f29875d60ef40564580c92b056308dd16
gabbykraemer <54597245+gabbykraemer@users.noreply.github.com>
2019-09-03 13:21:41 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup(){
+    createCanvas (1265,670);
+    background(0);
+    fill(255);
+    strokeWeight(10);
+
+    noStroke();
+  ellipse(633,223,70,70);
+  ellipse(633,240,150,30);
+  ellipse(633,300,10,200);
+  ellipse(600,450,10,400);
+  ellipse(650,300,10,100);
+  ellipse(675,430,10,400);
+  ellipse(600,100,30,30);
+  ellipse(700,223,20,20);
+  ellipse(1000,150,10,10);
+  ellipse(600,400,30,30);
+  ellipse(400,150,10,10);
+  ellipse(645,600,20,20);
+  ellipse(300,400,50,50);
+  ellipse(300,400,30,30);
+  ellipse(800,150,10,10);
+
+}
+function draw(){
+}
